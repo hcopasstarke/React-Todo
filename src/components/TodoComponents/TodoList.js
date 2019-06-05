250dade0c4671e543007a67321c4097a58b205b5
hcopasstarke <hcopasstarke@gmail.com>
2019-06-05 15:58:38 -0700

    branch + mvp

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const List = props => {
+  return (
+    <div>
+      {props.todoData.map((item, index) => {
+        return <Todo task={item} key={item.task} taskIndex={index} toggleTodo={props.toggleTodo} />;
+    })}
+    </div>
+  );
+}
+
+export default List;
